58ad207bb3faba0b882030dd4d9fd65928476d0a
koalkam <96691108+koalkam@users.noreply.github.com>
2022-08-23 14:06:14 +0300

    Update main.py
    now it creates an empty window

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from sys import exit
+
+# variables
+INITIAL_WINDOW_WIDTH = 1300
+INITIAL_WINDOW_HEIGHT = 700
+
+# main window class
+
+
+class IDE(QMainWindow):
+    def __init__(self):
+        super(IDE, self).__init__()
+
+        self.setGeometry(0, 0, INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT)
+
+        self.show()
+
+
+# running the app
+app = QApplication([])
+window = IDE()
+exit(app.exec())
+
